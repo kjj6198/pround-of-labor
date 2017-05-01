b8ce5be21e72fc59bd1150a837e66ec905f6bdb6
Kalan <kjj6198@gmail.com>
2017-05-02 01:06:38 +0800

    add procession image.

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -1,6 +1,16 @@
 import "owl.carousel";
 import { scrollToTarget } from './utils';
 
+const processions = [
+  'procession-1.jpg',
+  'procession-2.jpg',
+  'procession-3.jpg',
+  'procession-4.jpg',
+  'procession-5.jpg',
+  'procession-6.jpg',
+  'procession-7.jpg',
+];
+
 const assets = [
   {
     text: '台灣勞工事件簿',
@@ -36,6 +46,43 @@ assets
     $('.labor-images__content').append(img);
 
   })
+
+processions
+  .map(img => {
+    return `
+    <div class="owl-lazy" data-src="/${img}">
+    </div>
+    `;
+  })
+  .forEach(img => {
+    $('.labor-images__content-2').append(img);
+  })
+
+$('.labor-images__content-2').owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  lazyLoad: true,
+  responsive: {
+    0: {
+      items: 1.5,
+      margin: 8,
+      stagePadding: 0
+    },
+    960: {
+      items: 2,
+      margin: 8,
+      stagePadding: 16
+    },
+    1200: {
+      items: 4,
+      margin: 8,
+      stagePadding: 16
+    } 
+
+  }
+})
 
 $('.labor-images__content').owlCarousel({
   loop: true,
